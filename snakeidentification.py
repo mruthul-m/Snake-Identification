e987b2f2ce53fdb59b6f0b55e07c7d3bcd8d64bc
mruthulsgit <memruthu@gmail.com>
2022-10-15 18:54:54 +0530

    all project is done and you ca use the model which has 90% accuracy

---
+++ snakeidentification.py
@@ -0,0 +1,223 @@
+
+import numpy as np
+import pandas as pd
+from pathlib import Path
+import os.path
+import matplotlib.pyplot as plt
+from IPython.display import Image, display
+import matplotlib.cm as cm
+
+import tensorflow as tf 
+
+import os
+import shutil
+from tqdm import tqdm
+from random import shuffle
+
+import cv2
+from glob import glob
+
+from tensorflow.keras import backend as K
+import random
+import albumentations as A
+from sklearn.model_selection import train_test_split, StratifiedKFold
+
+from tensorflow.keras.layers import *
+from tensorflow.keras.optimizers import *
+from tensorflow.keras.models import *
+from tensorflow.keras.preprocessing.image import *
+from tensorflow.keras.callbacks import *
+from tensorflow.keras.applications.efficientnet import *
+
+image_dir = Path('/content/drive/MyDrive/mini project/Snake Images')
+
+# Get filepaths and labels
+# Get filepaths and labels
+filepaths = list(image_dir.glob(r'**/*.jpg'))
+labels = list(map(lambda x: os.path.split(os.path.split(x)[0])[1], filepaths))
+
+filepaths = pd.Series(filepaths, name='Filepath').astype(str)
+labels = pd.Series(labels, name='Label')
+
+# Concatenate filepaths and labels
+image_df = pd.concat([filepaths, labels], axis=1)
+
+image_df.head(5)
+
+# Shuffle the DataFrame and reset index
+image_df = image_df.sample(frac=1).reset_index(drop = True)
+
+# Show the result
+image_df.head(5)
+
+# Display 20 picture of the dataset with their labels
+fig, axes = plt.subplots(nrows=3, ncols=5, figsize=(15, 7),
+                        subplot_kw={'xticks': [], 'yticks': []})
+
+for i, ax in enumerate(axes.flat):
+    ax.imshow(plt.imread(image_df.Filepath[i]))
+    ax.set_title(image_df.Label[i])
+plt.tight_layout()
+plt.show()
+
+train_df, test_df = train_test_split(image_df, train_size=0.9, shuffle=True, random_state=1)
+
+train_df.shape
+
+train_generator = tf.keras.preprocessing.image.ImageDataGenerator(
+    preprocessing_function=tf.keras.applications.mobilenet_v2.preprocess_input,
+    validation_split=0.2
+)
+
+test_generator = tf.keras.preprocessing.image.ImageDataGenerator(
+    preprocessing_function=tf.keras.applications.mobilenet_v2.preprocess_input
+)
+
+train_df.head(5)
+
+train_images = train_generator.flow_from_dataframe(
+    dataframe=train_df,
+    x_col='Filepath',
+    y_col='Label',
+    target_size=(224, 224),
+    color_mode='rgb',
+    class_mode='categorical',
+    batch_size=32,
+    shuffle=True,
+    seed=42,
+    subset='training'
+)
+
+val_images = train_generator.flow_from_dataframe(
+    dataframe=train_df,
+    x_col='Filepath',
+    y_col='Label',
+    target_size=(224, 224),
+    color_mode='rgb',
+    class_mode='categorical',
+    batch_size=32,
+    shuffle=True,
+    seed=42,
+    subset='validation'
+)
+
+test_images = test_generator.flow_from_dataframe(
+    dataframe=test_df,
+    x_col='Filepath',
+    y_col='Label',
+    target_size=(224, 224),
+    color_mode='rgb',
+    class_mode='categorical',
+    batch_size=32,
+    shuffle=False
+)
+
+test_images
+
+def create_model(input_shape=(224, 224, 3)):
+    
+    inputs = Input(input_shape)
+    base_model = EfficientNetB1(input_shape=input_shape, include_top=False, classes=2)
+    
+    x = base_model(inputs)
+    
+    x = GlobalAveragePooling2D()(x)
+#     x = Dropout(0.1)(x)
+    
+    x = Dense(56, activation='relu')(x)
+    x = Dropout(0.1)(x)
+    
+    outputs = Dense(2, activation='sigmoid')(x)
+    
+    model = Model(inputs, outputs)
+    
+    return model
+
+K.clear_session()
+
+model = create_model((224, 224, 3))
+# model = load_model('models/checkpoint/EfficientNetB0.h5')
+
+metrics = [
+    'accuracy',
+    'AUC'
+]
+
+model.compile(optimizer=Adam(), loss='categorical_crossentropy', metrics=metrics)
+
+checkpoint_path = 'models/EfficientNetB1/model_224.h5'
+
+callbacks = [
+    EarlyStopping(monitor='val_loss', mode='min', patience=15, verbose=1),
+    ReduceLROnPlateau(monitor='val_loss', mode='min', factor=0.1, patience=5, min_lr=0.000001, verbose=1),
+    ModelCheckpoint(monitor='val_loss', mode='min', filepath=checkpoint_path, verbose=1, save_best_only=True, save_weights_only=False)
+]
+
+train_images
+
+
+
+history = model.fit(
+    train_images,
+    validation_data=val_images,
+    epochs=25,
+    callbacks=callbacks
+)
+
+model.save("/content/drive/MyDrive/mini project/modelnew.h5")
+
+pd.DataFrame(history.history)[['accuracy','val_accuracy']].plot()
+plt.title("Accuracy")
+plt.show()
+
+results = model.evaluate(test_images, verbose=0)
+print("    Test Loss: {:.5f}".format(results[0]))
+print("Test Accuracy: {:.2f}%".format(results[1] * 100))
+
+test_images
+
+# Predict the label of the test_images
+pred = model.predict(test_images)
+pred = np.argmax(pred,axis=1)
+
+# Map the label
+labels = (train_images.class_indices)
+labels = dict((v,k) for k,v in labels.items())
+pred = [labels[k] for k in pred]
+
+# Display the result
+print(f'The first 5 predictions: {pred[:5]}')
+
+
+
+image_dir = Path('/content/drive/MyDrive/SNAKE_TEST/Snake Images/valid')
+
+# Get filepaths and labels
+filepaths = list(image_dir.glob(r'**/*.jpg'))
+labels = list(map(lambda x: os.path.split(os.path.split(x)[0])[1], filepaths))
+
+filepaths = pd.Series(filepaths, name='Filepath').astype(str)
+labels = pd.Series(labels, name='Label')
+
+# Concatenate filepaths and labels
+valid_df = pd.concat([filepaths, labels], axis=1)
+
+test = test_generator.flow_from_dataframe(
+    dataframe=valid_df,
+    x_col='Filepath',
+    y_col='Label',
+    target_size=(224, 224),
+    color_mode='rgb',
+    class_mode='categorical',
+    batch_size=32,
+    shuffle=False
+)
+
+pred = model.predict(test)
+pred = np.argmax(pred,axis=1)
+
+if pred[0]==1:
+  print("venomous")
+else:
+  print("non venomous")
+
